b0c558dbae65ad20a6fc5e57050a44dedbf7a783
David Edwards <david@more.fool.me.uk>
2014-11-28 15:11:38 +0000

    Add script for building Spigot from source

---
+++ build-spigot.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir -p .spigot/build
+cd .spigot/build
+
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -O BuildTools.jar
+
+java -jar BuildTools.jar
+
+cd ../..
+
+mkdir -p .spigot/jars
+
+cp .spigot/build/Spigot/Spigot-Server/target/spigot*.jar .spigot/jars
+
+
